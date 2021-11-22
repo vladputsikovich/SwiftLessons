38267a306fd6a7bfb92dd6a0fbd66acaaa4092f7
vladputsikovich <vladputsikovich@gmail.com>
2021-11-22 13:44:46 +0000

    Fix some

--- main.swift
+++ main.swift
@@ -10,7 +10,7 @@ class Human {
   var weight : Double
   var sex : Sex
 
-  init (name:String,height: Double, weight:Double, sex:Sex){
+  init (name: String,height: Double, weight: Double, sex: Sex){
     self.name = name
     self.height = height
     self.weight = weight
@@ -22,76 +22,76 @@ class Human {
   }
 }
 
-class Runner:Human{
+class Runner: Human {
   override func move(){
     print("Бегун бежит")
   }
 }
 
-class Swimer:Human{
-  override func move(){
+class Swimer: Human {
+  override func move() {
     print("Плавец плывет")
   }
 }
 
-class Jumper : Human {
-  var female : String
-  init (name:String,height: Double, weight:Double, sex:Sex, female:String){
+class Jumper: Human {
+  var female: String
+  init (name: String, height: Double, weight: Double, sex: Sex, female: String) {
     self.female = female
-    super.init(name:name,height:height,weight:weight,sex:sex)
+    super.init(name: name, height: height, weight: weight, sex: sex)
   }
-  override func move(){
+  override func move() {
       super.move()
       print("Прыгун прыгает")
     }
 }
 
-class Animal{
+class Animal {
   var nickname: String
   var years: Int
 
-  init(nickname:String, years: Int){
+  init(nickname: String, years: Int) {
     self.nickname = nickname
     self.years = years
   }
-  func move(){
+  func move() {
     print("Животное бежит")
   }
 }
 
-class Wolf:Animal{
-  override func move(){
+class Wolf: Animal {
+  override func move() {
     print("Волк бежит")
   }
 }
 
-class Tiger:Animal{
-  override func move(){
+class Tiger: Animal {
+  override func move() {
     print("Тигр бежит")
   }
 }
 
-var vlad = Human(name:"Влда", height: 10.2,weight: 20.4,sex: Sex.man)
-var sasha = Runner(name:"Саша", height: 10.2,weight: 20.4,sex: Sex.man)
-var dima = Swimer(name:"Дима", height: 10.2,weight: 20.4,sex: Sex.man)
-var oleg = Jumper(name:"Олег", height: 10.2,weight: 20.4,sex: Sex.man,female:"Витальевич")
+var vlad = Human(name: "Влда", height: 10.2, weight: 20.4, sex: Sex.man)
+var sasha = Runner(name: "Саша", height: 10.2, weight: 20.4, sex: Sex.man)
+var dima = Swimer(name: "Дима", height: 10.2, weight: 20.4, sex: Sex.man)
+var oleg = Jumper(name: "Олег", height: 10.2, weight: 20.4, sex: Sex.man, female: "Витальевич")
 
-var anim = Animal(nickname:"Пуц",years:24)
-var tig = Tiger(nickname:"REy",years:24)
-var wolf = Wolf(nickname:"РОК",years:24)
+var anim = Animal(nickname: "Пуц", years: 24)
+var tig = Tiger(nickname: "REy", years: 24)
+var wolf = Wolf(nickname: "РОК", years: 24)
 
-var peoples:[Any] = [vlad, sasha, dima, oleg, anim, tig, wolf]
+var peoples: [AnyObject] = [vlad, sasha, dima, oleg, anim, tig, wolf]
 
 peoples.reverse()
 
-for man in peoples {
-  if man is Human{
-    print("Имя: \((man as! Human).name) Рост: \((man as! Human).height) Вес: \((man as! Human).weight) Пол: \((man as! Human).sex)")
-    (man as! Human).move()
+peoples.forEach{man in 
+  if let man = man as? Human {
+    print("Имя: \(man.name) Рост: \(man.height) Вес: \(man.weight) Пол: \(man.sex)")
+    man.move()
   }
-  if man is Animal{
-    print("Кличка: \((man as! Animal).nickname) Возраст: \((man as! Animal).years)")
-    (man as! Animal).move()
+  if let man = man as? Animal {
+    print("Кличка: \(man).nickname) Возраст: \(man.years)")
+    man.move()
   }
   print("\n")
 }
